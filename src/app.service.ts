1b9e6cbf79994e4f5502a1148491335007f2d214
Ivan Burzilov <burzilov@ya.ru>
2021-09-01 16:40:28 +0300

    Debug

--- src/app.service.ts
+++ src/app.service.ts
@@ -40,7 +40,10 @@ export class GeocodeService {
     // поиск стран и городов по почтовому индексу с помощью Geonames.Org
 
     if (!query) {
-      return of([]);
+      throw new HttpException(
+        { error: 'Empty query' },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
 
     const url =
@@ -80,7 +83,10 @@ export class GeocodeService {
     // поиск стран и городов по почтовому индексу с помощью Google Maps
 
     if (!query) {
-      return of([]);
+      throw new HttpException(
+        { error: 'Empty query' },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
 
     const url =
@@ -93,11 +99,8 @@ export class GeocodeService {
       map((resp: any) => {
         if (resp.data.error_message) {
           throw new HttpException(
-            {
-              status: HttpStatus.INTERNAL_SERVER_ERROR,
-              error: resp.data.error_message,
-            },
-            HttpStatus.FORBIDDEN,
+            { error: resp.data.error_message },
+            HttpStatus.INTERNAL_SERVER_ERROR,
           );
         }
         return resp.data.predictions
@@ -154,6 +157,148 @@ export class GeocodeService {
             address.zip,
             address.city,
           ].join(', ');
+          return address;
+        });
+      }),
+    );
+  }
+
+  addressGoogleCom(
+    query: string,
+    country_iso: string,
+    zip: string,
+    city: string,
+  ): Observable<Address[]> {
+    // поиск адреса с помощью Google Maps
+
+    if (!query) {
+      throw new HttpException(
+        { error: 'Empty query' },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+
+    const url =
+      'https://maps.googleapis.com/maps/api/place/autocomplete/json?language=en&key=' +
+      this.options.google_api_key +
+      '&input=' +
+      encodeURIComponent(`${zip}, ${city}, ${query}`) +
+      '&components=country:' +
+      country_iso;
+
+    return this.http.get(url).pipe(
+      map((resp: any) => {
+        if (resp.data.error_message) {
+          throw new HttpException(
+            { error: resp.data.error_message },
+            HttpStatus.INTERNAL_SERVER_ERROR,
+          );
+        }
+        return (resp.data.predictions as any[])
+          .map((x: any) => {
+            return {
+              place_id: x.place_id,
+              types: x.types,
+              url:
+                'https://maps.googleapis.com/maps/api/place/details/json?language=en&key=' +
+                this.options.google_api_key +
+                '&place_id=' +
+                x.place_id,
+            };
+          })
+          .filter(
+            (x: any) =>
+              (x.types as any[]).includes('street_address') ||
+              (x.types as any[]).includes('route'),
+          )
+          .map((x: any) => {
+            return this.http.get(x.url);
+          });
+      }),
+
+      switchMap((x) => forkJoin(x)),
+
+      map((resp: any[]) => {
+        return resp.map((x: any) => {
+          const components = x.data.result.address_components;
+          const address = {
+            suggestion: x.data.result.formatted_address,
+            street: '',
+            house: '',
+          } as Address;
+          components.forEach((x: any) => {
+            const t = x.types as string[];
+            if (t.includes('route')) {
+              address.street = x.long_name;
+            }
+            if (t.includes('street_number')) {
+              address.house = x.long_name;
+            }
+          });
+          return address;
+        });
+      }),
+    );
+  }
+
+  normalizeGoogleCom(address: string): Observable<Address> {
+    // нормализация адреса с помощью Google Maps
+    if (!address) {
+      return of(null);
+    }
+
+    const url =
+      'https://maps.googleapis.com/maps/api/geocode/json?key=' +
+      this.options.google_api_key +
+      '&address=' +
+      encodeURIComponent(address);
+
+    return this.http.get(url).pipe(
+      map((resp: any) => {
+        if (resp.data.error_message) {
+          throw new HttpException(
+            { error: resp.data.error_message },
+            HttpStatus.INTERNAL_SERVER_ERROR,
+          );
+        }
+        if (!resp.data?.results?.length) {
+          return of(null);
+        }
+        return resp.data?.results.map((x: any) => {
+          const components = x.address_components;
+          const address = {} as Address;
+          components.forEach((x: any) => {
+            const t = x.types as string[];
+            if (t.includes('postal_code')) {
+              address.zip = x.long_name;
+            }
+            if (t.includes('country')) {
+              address.country_iso = x.short_name;
+              address.country = x.long_name;
+            }
+            if (t.includes('locality')) {
+              address.city = x.long_name;
+            }
+            if (t.includes('administrative_area_level_3') && !address.city) {
+              address.city = x.long_name;
+            }
+            if (t.includes('administrative_area_level_2') && !address.city) {
+              address.city = x.long_name;
+            }
+            if (t.includes('administrative_area_level_1') && !address.city) {
+              address.city = x.long_name;
+            }
+            if (t.includes('route')) {
+              address.street = x.long_name;
+            }
+            if (t.includes('street_number')) {
+              address.house = x.long_name;
+            }
+          });
+          if (address.city === '') {
+            address.city = address.country;
+          }
+          address.suggestion = x.formatted_address;
           return address;
         });
       }),
