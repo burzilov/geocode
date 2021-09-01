7ff706bbbb7ca727ffcb1f68fd7c95d1026a6d39
Ivan Burzilov <burzilov@ya.ru>
2021-09-01 12:36:54 +0300

    Debug

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,8 +2,12 @@ import { Inject, Injectable } from '@nestjs/common';
 import { OPTIONS } from './app.module';
 import { GeocodeOptions } from './options.interface';
 
+interface IGeocodeService {
+  test(): Promise<any>;
+}
+
 @Injectable()
-export class GeocodeService {
+export class GeocodeService implements IGeocodeService {
   constructor(@Inject(OPTIONS) private options: GeocodeOptions) {}
 
   async test(): Promise<any> {
