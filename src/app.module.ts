a64a66c1153fddbb1d84f50032fdb37f21127246
Ivan Burzilov <burzilov@ya.ru>
2021-09-01 12:17:27 +0300

    Debug

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ export const OPTIONS = 'OPTIONS';
 
 @Global()
 @Module({
-  exports: [GeocodeService, OPTIONS],
+  exports: [GeocodeService],
   providers: [GeocodeService],
 })
 export class GeocodeModule {
