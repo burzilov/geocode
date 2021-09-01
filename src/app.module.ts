087dcb67f1f92d2a63b27c2ee018d7fa03caaedb
Ivan Burzilov <burzilov@ya.ru>
2021-09-01 13:39:26 +0300

    Debug

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
+import { HttpModule } from '@nestjs/axios';
 import { DynamicModule, Global, Module } from '@nestjs/common';
 import { GeocodeOptions, OPTIONS } from './app.interfaces';
 import { GeocodeService } from './app.service';
 
 @Global()
 @Module({
-  providers: [GeocodeService],
+  imports: [HttpModule],
   exports: [GeocodeService],
+  providers: [GeocodeService],
 })
 export class GeocodeModule {
   public static register(options: GeocodeOptions): DynamicModule {
